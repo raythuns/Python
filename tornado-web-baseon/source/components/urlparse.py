c110b2a315658e540e3abb1a5e63a9d3769a4f96
raythuns <raythunschen@qq.com>
2018-03-22 22:09:17 +0800

    useful parser

---
+++ tornado-web-baseon/source/components/urlparse.py
@@ -0,0 +1,13 @@
+from tornado.web import url
+
+
+def urlparse(rule, shadow_list):
+    rst = []
+    for s in shadow_list:
+        if (len(s) > 2 and isinstance(s[2], str)) or\
+                (len(s) > 3 and isinstance(s[2], str)):
+            rst.append(url(r'%s/%s' % (rule, s[0]),
+                           *s[1:-1], name=s[-1]))
+        else:
+            rst.append((r'%s/%s' % (rule, s[0]), *s[1:]))
+    return rst
